8def6ec04f02e07487e50d1e50a6fea3f6ef0965
Mina-Melad-1 <menamelad586@gmail.com>
2025-11-26 17:31:08 +0200

    Login, Logout endpoints

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -1,9 +1,12 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Http\Request;
 use App\Models\User;
 use Illuminate\Support\Facades\Hash;
 use App\Http\Requests\RegisterRequest;
+use Illuminate\Support\Facades\Auth;
+
 class AuthController extends Controller
 {
     public function register(RegisterRequest $request)
@@ -21,5 +24,36 @@ class AuthController extends Controller
             'email' => $user->email,
             'password' => $user->password
         ], 201);
+    }
+
+        public function login(Request $request)
+    {
+        $request->validate([
+            "email"    => "required|string|email",
+            "password" => "required|string",
+        ]);
+
+        if(!Auth::attempt($request->only('email', 'password'))){
+            return response()->json([
+                'message' => 'Invalid login details'
+            ], 401);
+        }
+        $user = User::where('email',$request->email)->firstOrFail();
+        $token = $user->createToken('auth_token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'User logged in successfully',
+            'user' => $user,
+            'token' => $token,
+        ], 200);
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'User logged out successfully'
+        ], 200);
     }
 }
