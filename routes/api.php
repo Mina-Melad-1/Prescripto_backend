0219e6e6baebd110f3ba04c552409763acac0717
Mina-Melad-1 <menamelad586@gmail.com>
2026-02-02 19:32:04 +0200

    Edit route from put to post

--- routes/api.php
+++ routes/api.php
@@ -14,13 +14,13 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::get('/profile', [AuthController::class, 'profile'])->middleware('auth:sanctum');
-Route::put('/editProfile', [AuthController::class, 'edit_profile'])->middleware('auth:sanctum');
+Route::post('/editProfile', [AuthController::class, 'edit_profile'])->middleware('auth:sanctum');
 
 Route::post('/admin/login', [AdminController::class, 'login']);
 Route::post('/admin/logout', [AdminController::class, 'logout'])->middleware('auth:sanctum', 'admin');
 Route::get('/admin/dashboard', [AdminController::class, 'dashboard'])->middleware('auth:sanctum', 'admin');
 Route::post('/admin/add-doctor', [AdminController::class, 'add_doctor'])->middleware('auth:sanctum', 'admin');
-Route::put('/admin/edit-doctor/{doctorId}', [AdminController::class, 'edit_doctor'])->middleware('auth:sanctum', 'admin');
+Route::post('/admin/edit-doctor/{doctorId}', [AdminController::class, 'edit_doctor'])->middleware('auth:sanctum', 'admin');
 Route::get('/admin/doctors', [AdminController::class, 'doctors'])->middleware('auth:sanctum', 'admin');
 
 
